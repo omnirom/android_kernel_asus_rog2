e0017edbfd3ed375090f335519e0450425f7fd0f
Daniel Vasquez <danielgusvt@yahoo.com>
2018-11-11 00:48:56 -0500

    fs: sdfat: Add config option to register sdFAT for texFAT
    This way, we are compatible with the tuxera solution of the exfat
    driver that the stock rom expects.
    
    Signed-off-by: Daniel Vasquez <danielgusvt@yahoo.com>
    (cherry picked from commit 91dd4ce1834db569a362bcbc5abfa6f81dcf4e3c)
    (cherry picked from commit 0f258846ad7f01f80c75f4ca94565ecfecd21e46)
    (cherry picked from commit 0b335af29e242fb0ab3f09a39a71d993506a09c7)

--- fs/sdfat/sdfat.c
+++ fs/sdfat/sdfat.c
@@ -5119,6 +5119,21 @@ static struct file_system_type vfat_fs_type = {
 MODULE_ALIAS_FS("vfat");
 #endif /* CONFIG_SDFAT_USE_FOR_VFAT */
 
+#ifdef CONFIG_SDFAT_COMPAT_TUXERA
+static struct file_system_type texfat_fs_type = {
+	.owner       = THIS_MODULE,
+	.name        = "texfat",
+	.mount       = sdfat_fs_mount,
+#ifdef CONFIG_SDFAT_DBG_IOCTL
+	.kill_sb    = sdfat_debug_kill_sb,
+#else
+	.kill_sb    = kill_block_super,
+#endif /* CONFIG_SDFAT_DBG_IOCTL */
+	.fs_flags    = FS_REQUIRES_DEV,
+};
+MODULE_ALIAS_FS("texfat");
+#endif /* CONFIG_SDFAT_COMPAT_TUXERA */
+
 static int __init init_sdfat_fs(void)
 {
 	int err;
@@ -5173,6 +5188,14 @@ static int __init init_sdfat_fs(void)
 	}
 #endif /* CONFIG_SDFAT_USE_FOR_VFAT */
 
+#ifdef CONFIG_SDFAT_COMPAT_TUXERA
+	err = register_filesystem(&texfat_fs_type);
+	if (err) {
+		pr_err("[SDFAT] failed to register for texfat filesystem\n");
+		goto error;
+	}
+#endif /* CONFIG_SDFAT_COMPAT_TUXERA */
+
 	return 0;
 error:
 	sdfat_statistics_uninit();
@@ -5209,6 +5232,9 @@ static void __exit exit_sdfat_fs(void)
 #ifdef CONFIG_SDFAT_USE_FOR_VFAT
 	unregister_filesystem(&vfat_fs_type);
 #endif /* CONFIG_SDFAT_USE_FOR_VFAT */
+#ifdef CONFIG_SDFAT_COMPAT_TUXERA
+	unregister_filesystem(&texfat_fs_type);
+#endif /* CONFIG_SDFAT_COMPAT_TUXERA */
 	fsapi_shutdown();
 }
 
