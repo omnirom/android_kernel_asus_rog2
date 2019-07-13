fab400deba9b784c714f763d9bbe489ec68843b1
GuaiYiHu <guaiyihu@foxmail.com>
2019-07-14 00:42:11 +0200

    kernel: module: Hack to force load prebuilt vendor modules
    Change-Id: I5a257fabe79520779c704f9760c7e2ebc48fabe6

--- kernel/module.c
+++ kernel/module.c
@@ -1271,7 +1271,7 @@ static int try_to_force_load(struct module *mod, const char *reason)
 #endif
 }
 
-#ifdef CONFIG_MODVERSIONS
+#if 0
 
 static u32 resolve_rel_crc(const s32 *crc)
 {
@@ -3691,6 +3691,9 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	struct module *mod;
 	long err;
 	char *after_dashes;
+
+	flags |= MODULE_INIT_IGNORE_MODVERSIONS;
+	flags |= MODULE_INIT_IGNORE_VERMAGIC;
 
 	err = module_sig_check(info, flags);
 	if (err)
